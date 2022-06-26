703d026d6d75b7ac77db558e0a946e0d8571f9da
omairalamada <omairmangondaya@gmail.com>
2022-06-26 23:39:42 +0800

    database connection added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { UsersModule } from './modules/users/users.module';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'user-management-db',
+      autoLoadEntities: true,
+      synchronize: true
+    })
+  ]
 })
 export class AppModule {}
