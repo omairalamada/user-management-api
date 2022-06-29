ff47ad1d0d2e6005ca7cbc46de4e33991781c98a
omairalamada <omairmangondaya@gmail.com>
2022-06-30 01:41:00 +0800

    fixed not found user.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,9 @@
+import { ErrorMessages } from './../../common/error-messages.enum';
 import { UserEntity } from './entities/user.entity';
 import { UserDto } from './dtos/user.dto';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOneOptions, Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService { 
@@ -11,7 +12,18 @@ export class UsersService {
         private readonly userRepo: Repository<UserEntity>,
     ) {}
 
+    findOne( findData: FindOneOptions<UserEntity>): Promise<Partial<UserEntity>> {
+        return this.userRepo.findOne(findData)
+    }
+
     async createUser(user: UserDto): Promise<UserEntity> {
+        const { email } = user
+        const found = await this.findOne({ where: { email }})
+
+        if (found && found.email === email) {
+            throw new BadRequestException(ErrorMessages.ALREADY_REGISTERED)
+        }
+
         const newUser = this.userRepo.create({
             ...user,
         })
