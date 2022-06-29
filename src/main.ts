40f8c26cca0433f7d9977c274238690a1e2e438a
omairalamada <omairmangondaya@gmail.com>
2022-06-29 23:09:14 +0800

    setup swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('User Management API')
+    .setDescription('User Managememt API')
+    .setVersion('1.0')
+    .addTag('User Management')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'Token',
+      },
+      'access-token',
+    )
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/documentation', app, document);
+
+  await app.listen(process.env.PORT || 3000, () => {
+
+    console.info(`Server details:
+    Port:${process.env.PORT || 3000}`)
+    
+  });
 }
 bootstrap();
