a070a7c89209cdb772045f1579583fc87b77bc9c
omairalamada <omairmangondaya@gmail.com>
2022-06-29 23:10:48 +0800

    create service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,11 @@
+import { UserEntity } from './entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
   providers: [UsersService]
 })
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,24 @@
+import { UserEntity } from './entities/user.entity';
+import { UserDto } from './dtos/user.dto';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService { 
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepo: Repository<UserEntity>,
+    ) {}
+
+    async createUser(user: UserDto): Promise<UserEntity> {
+        const newUser = this.userRepo.create({
+            ...user,
+        })
+
+        const saved: UserEntity = await this.userRepo.save(newUser);
+
+        return saved;
+    }
+
+}
