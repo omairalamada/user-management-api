1b98f639035bf6968a4589c5fae4f75c4a712d68
omairalamada <omairmangondaya@gmail.com>
2022-06-30 03:31:48 +0800

    add service for login

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,12 @@
+import { LoginUserDto } from './dtos/login-user.dto';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../users/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity, LoginUserDto])],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { UsersService } from './../users/users.service';
+import { LoginUserDto } from './dtos/login-user.dto';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../users/entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userService: UsersService
+    ) {}
+    
+
+    async signIn(loginUser: LoginUserDto): Promise<string> {
+        const { username, password } = loginUser;
+
+        const user = await this.userService.findOne({ where: { username }});
+
+        if( user && (await bcrypt.compare(password, user.password))) {
+            return 'success';
+        } else {
+            throw new UnauthorizedException('Please check your credential');
+        }
+    }
+}
