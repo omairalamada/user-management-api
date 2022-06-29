658e32d50b6f55e4365a3177955a3bc8dbcd2106
omairalamada <omairmangondaya@gmail.com>
2022-06-30 02:54:03 +0800

    generate hash and salt for security

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,6 +4,7 @@ import { UserDto } from './dtos/user.dto';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FindOneOptions, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService { 
@@ -17,15 +18,27 @@ export class UsersService {
     }
 
     async createUser(user: UserDto): Promise<UserEntity> {
-        const { email } = user
+        const { firstName, lastName, address, postCode, contactNumber, email, username, password } = user
         const found = await this.findOne({ where: { email }})
+
+        // hash
+        const salt = await bcrypt.genSalt();
+        // generate hash password
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         if (found && found.email === email) {
             throw new BadRequestException(ErrorMessages.ALREADY_REGISTERED)
         }
 
         const newUser = this.userRepo.create({
-            ...user,
+            firstName,
+            lastName,
+            address,
+            postCode,
+            contactNumber,
+            email,
+            username,
+            password: hashedPassword,
         })
 
         const saved: UserEntity = await this.userRepo.save(newUser);
