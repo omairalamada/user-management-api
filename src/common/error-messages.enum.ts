c336bd04ccdbf05d56c42de39c15e91cec7de7d9
omairalamada <omairmangondaya@gmail.com>
2022-06-30 01:38:44 +0800

    error message enum

---
+++ src/common/error-messages.enum.ts
@@ -0,0 +1,7 @@
+export enum ErrorMessages {
+    INVALID_CREDENTIALS = 'Invalid email or password',
+    UNVERIFIED_EMAIL = 'Please verify your email',
+    ALREADY_REGISTERED = 'Email is already registered',
+    EXPIRED_LINK = 'Link expired',
+    EMAIL_ALREADY_VERIFIED = 'Email already verified',
+}
