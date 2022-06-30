46ab638c8eef1fed964248da72da01fb721f4fe1
omairalamada <omairmangondaya@gmail.com>
2022-06-30 23:04:23 +0800

    add controller edit and get all users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,10 @@
 import { ResponseDto } from './../../common/dto/response.dto';
 import { UserEntity } from './entities/user.entity';
 import { UsersService } from './users.service';
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserDto } from './dtos/user.dto';
-import { RegisterDocDecorator } from './register-doc.decorator';
+import { EditUserDocDecorator, GetAllUsersDoc, RegisterUserDocDecorator } from './swagger/user-doc.decorator';
 
 @ApiTags('Users')
 @Controller('users')
@@ -12,17 +12,33 @@ export class UsersController {
     constructor(private readonly userService: UsersService) {}
 
     @Post('/register')
-    @RegisterDocDecorator()
+    @RegisterUserDocDecorator()
     async createUser(@Body() userDto: UserDto): Promise<ResponseDto<UserEntity>> {
         const response = await this.userService.createUser(userDto);
 
         return { data: response }
     }   
 
+    @ApiOperation({ summary: 'Delete user by id', operationId: 'DeleteUser' })
+    @ApiResponse({ status: 200, type: UserDto })
     @Delete(':id')
     async deleteUser(@Param('id') id: number) {
-        const response = await this.userService.deleteUser(id);
+        return await this.userService.deleteUser(id);
+    }
 
-        return 'Successfully deleted!';
+    @Get('GetAllUsers')
+    @GetAllUsersDoc()
+    async getAllUsers(): Promise<ResponseDto<UserEntity[]>> {
+        const response = await this.userService.getAllUsers();
+
+        return { data: response }
+    }
+
+    @Patch('edit/:id')
+    @EditUserDocDecorator()
+    async editUser(@Param('id') id: number, @Body() userDto: UserDto): Promise<ResponseDto<UserEntity>> {
+        const response = await this.userService.editUser(id, userDto);
+
+        return { data: response };
     }
 }
