a5199fcff396ed41d2ef7b7a3ce39f274159c291
omairalamada <omairmangondaya@gmail.com>
2022-06-30 14:06:21 +0800

    initialize some class for JWT

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,25 @@
+import { JwtStrategy } from './jwt.strategy';
 import { LoginUserDto } from './dtos/login-user.dto';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '../users/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, LoginUserDto])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600, // 1 hour expire
+      }
+    }),
+    TypeOrmModule.forFeature([UserEntity, LoginUserDto])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
