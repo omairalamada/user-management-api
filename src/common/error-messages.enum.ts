21e8530d3b1b73d760d103f809d2241f73ce5fb2
omairalamada <omairmangondaya@gmail.com>
2022-07-03 01:58:32 +0800

    modified

--- src/common/error-messages.enum.ts
+++ src/common/error-messages.enum.ts
@@ -1,7 +1,6 @@
 export enum ErrorMessages {
     INVALID_CREDENTIALS = 'Invalid email or password',
     UNVERIFIED_EMAIL = 'Please verify your email',
-    ALREADY_REGISTERED = 'Email is already registered',
-    EXPIRED_LINK = 'Link expired',
-    EMAIL_ALREADY_VERIFIED = 'Email already verified',
+    ALREADY_REGISTERED = 'Email is already registered.',
+    USERNAME_USED = 'Username is already used.'
 }
