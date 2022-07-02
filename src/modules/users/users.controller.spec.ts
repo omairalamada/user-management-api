ac5f52a7eabd8c8dc10c4516970199d9eb8a51db
omairalamada <omairmangondaya@gmail.com>
2022-07-03 01:33:04 +0800

    unit test for every controller method.

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,18 +1,69 @@
+import { UserDto } from './dtos/user.dto';
+import { UsersService } from './users.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let userController: UsersController;
+  let userService: UsersService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    const ApiServiceProvider = {
+      provide: UsersService,
+      useFactory: () => ({
+        createUser: jest.fn(() => []),
+        editUser: jest.fn(() => []),
+        deleteUser: jest.fn(() => []),
+        getAllUsers: jest.fn(() => []),
+      })
+    }
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService, ApiServiceProvider],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    userController = module.get<UsersController>(UsersController);
+    userService = module.get<UsersService>(UsersService)
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
   });
+
+  it("should call createUser controller method to create new user", () => {
+    const userDto = new UserDto();
+    expect(userController.createUser(userDto)).not.toEqual(null);
+  })
+
+  it('should call createUser for same parameters', () => {
+    const userDto = new UserDto();
+
+    userController.createUser(userDto);
+    expect(userService.createUser).toHaveBeenCalled();
+    expect(userService.createUser).toBeCalledWith(userDto);
+  })
+
+  it('should call editUser method', () => {
+    const userDto = new UserDto();
+    const userId = userDto.id;
+    userController.editUser(userId, userDto);
+
+    expect(userService.editUser).toHaveBeenCalled();
+  })
+
+  it("should call deleteUser controller method to delete user by id", () => {
+    const userDto = new UserDto();
+
+    const userId = userDto.id = 1;
+    userController.deleteUser(userId);
+
+    expect(userService.deleteUser).toHaveBeenCalled();
+  })
+
+  it('should call getAllUsers controller method to display users', () => {
+    userController.getAllUsers();
+    expect(userService.getAllUsers).toHaveBeenCalled();
+  })
+
 });
